cdedc374144c87fd5b8f2f534d512a5af9e23258
Nopi178z <nopi1782006@gmail.com>
2025-07-01 23:16:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@
 
 
 module.exports = {
-    token: 'MTM4OTYzNTE4MTcyMzI1NDg5NQ.Gb82l2.wowdLWkc0Z3-yyqWvChZ8E5FkgorIZmAjXwR4g',
+    token: '',
     guildId: '1294229874659164210',
     mongodbURI: 'mongodb+srv://nopi:nopi@discordbot.ly6shka.mongodb.net/?retryWrites=true&w=majority'
 };
